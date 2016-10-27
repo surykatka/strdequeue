50771717d1ad845076883340a3170aafde919a8e
Agnieszka Strzałka <as320805@mimuw.edu.pl>
2016-10-27 21:44:13 +0200

    Dodanie testów z flagą NDEBUG

--- test1.sh
+++ test1.sh
@@ -1,4 +1,4 @@
-g++ -c -Wall -O2 -std=c++1y strdeque.cc -o strdeque.o
-g++ -c -Wall -O2 -std=c++1y strdequeconst.cc -o strdequeconst.o
-gcc -c -Wall -O2 strdeque_test1.c -o strdeque_test1.o
+g++ -c -DNDEBUG -Wall -O2 -std=c++1y strdeque.cc -o strdeque.o
+g++ -c -DNDEBUG -Wall -O2 -std=c++1y strdequeconst.cc -o strdequeconst.o
+gcc -c -DNDEBUG -Wall -O2 strdeque_test1.c -o strdeque_test1.o
 g++ strdeque_test1.o strdeque.o strdequeconst.o -o strdeque_test1
